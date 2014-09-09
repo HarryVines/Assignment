a688a5218470c9bb7fb80d76b7159931822356fb
44550 <44550@PC004146.coll.lr.local>
2014-09-09 12:53:07 +0100

    Input Command (Updated)

--- First_Name.py
+++ First_Name.py
@@ -1,4 +1,4 @@
-print("Hello World")
-first_name = input("Please enter your name:")
-print(first_name)
-print("hi {0}!".format(first_name))
+print("Hello World")#Tell the computer to print "Hello World"
+first_name = input("Please enter your name:")#Tell the computer to say "Please enter name" and prompts the user to enter a name
+print(first_name)#Prints the variable that is associated with that name
+print("hi {0}!".format(first_name))#Prints "hi" and the variable associated with first_name but associates the first_name to the {0}
